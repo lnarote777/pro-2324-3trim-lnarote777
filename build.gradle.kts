b368ca2463f6f9ca904116aec212d3bf3ec947dd
lnarote777 <lnarote777@iesrafaelalberti.es>
2024-05-14 17:44:06 +0200

    dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,17 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+
+    testImplementation(kotlin("test"))
+
+    //DCS: Base de datos H2
+    implementation("com.h2database:h2:2.2.224")
+
+    //DCS: HikariCP
+    implementation ("com.zaxxer:HikariCP:5.0.0")
+
+    //DCS: Arregla el warning SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder".
+    implementation("org.slf4j:slf4j-nop:2.0.6")
 }
 
 compose.desktop {
